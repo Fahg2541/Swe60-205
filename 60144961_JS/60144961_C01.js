1de9857090c9533d07cc620633412125ea1c4a2b
fahg2541 <fahg2541@gmail.com>
2020-02-26 19:59:09 +0700

    add

---
+++ 60144961_JS/60144961_C01.js
@@ -0,0 +1,23 @@
+   function programer1() {
+        alert("ID: 60144961\nName: Saowarat Chuandee");
+        
+    }
+    function programer2() {
+        alert("ID: 60144235\nName: Sirirat Wichityeam");
+        
+    }
+    function Name() {
+        var name = document.getElementById("h_name_id").value;
+    alert(name);
+        
+    }
+    function Nameresult() {
+        var nameresult = document.getElementById("h_name_id").value;
+        document.getElementById("h_result_id").value = nameresult
+    // alert(nameresult);
+    }
+    function Add() {
+        
+        
+    }
+    
